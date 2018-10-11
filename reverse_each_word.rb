ef8ffa7b990d541fa14970a676c55c37460aa8e7
Linda Chang <lindachang186@gmail.com>
2018-10-11 18:37:50 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,12 @@
 def reverse_each_word(sentence)
     reverse = []
+    reverse.push(sentence.split(" "))
+    reverse.collect do |word|
+      word.reverse!
+    end
     reverse.push(sentence)
     reverse.collect do |word|
       word.reverse
     end
-    reverse.push(sentence.split(" "))
-    reverse.collect do |word|
-      word.reverse!
-      return reverse
-    end
+
   end
